1ea425912bc519ab5a50e0125a20e88f00b684a7
eliasaboufarah02 <72624529+eliasaboufarah02@users.noreply.github.com>
2022-04-18 15:03:56 -0400

    Initialisation

---
+++ JukeBox-Backend/src/main/java/jukeBox/JukeBoxBackend/JukeBoxBackendApplication.java
@@ -0,0 +1,13 @@
+package jukeBox.JukeBoxBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JukeBoxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JukeBoxBackendApplication.class, args);
+	}
+
+}
---
+++ JukeBox-Backend/src/test/java/jukeBox/JukeBoxBackend/JukeBoxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package jukeBox.JukeBoxBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JukeBoxBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
