1497772eea7b21a22e120e007e25d6afd76ce267
eliasaboufarah02 <72624529+eliasaboufarah02@users.noreply.github.com>
2022-04-18 17:08:30 -0400

    Build succesfull

--- JukeBox-Backend/src/main/java/jukeBox/JukeBoxBackend/JukeBoxBackendApplication.java
+++ JukeBox-Backend/src/main/java/jukeBox/JukeBoxBackend/JukeBoxBackendApplication.java
@@ -2,12 +2,18 @@ package jukeBox.JukeBoxBackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RestController
 @SpringBootApplication
 public class JukeBoxBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JukeBoxBackendApplication.class, args);
 	}
-
+	@RequestMapping("/")
+	public String greeting(){
+		return "Hello world!";
+	}
 }
