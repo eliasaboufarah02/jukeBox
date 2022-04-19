08c4e756ed837c13156e6709c17ca022fcc0d18d
eliasaboufarah02 <72624529+eliasaboufarah02@users.noreply.github.com>
2022-04-19 04:09:41 -0400

    I/O data parsing/formatting

--- JukeBox-Backend/src/main/java/jukeBox/JukeBoxBackend/controller/JukeBoxController.java
+++ JukeBox-Backend/src/main/java/jukeBox/JukeBoxBackend/controller/JukeBoxController.java
@@ -1,20 +1,23 @@
 package jukeBox.JukeBoxBackend.controller;
 
-import java.io.IOException;
+import java.io.BufferedReader;
+
 import java.io.InputStream;
+
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
+
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.List;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.h2.H2ConsoleProperties.Settings;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import jukeBox.JukeBoxBackend.dto.JukeBoxDto;
@@ -29,7 +32,7 @@ public class JukeBoxController {
 	private JukeBoxService service;
 	
 	@GetMapping(value = { "/jukes", "/jukes/" })
-	public ResponseEntity<?> getAllAccounts() {
+	public ResponseEntity<?> getJukeBoxes(@RequestParam(defaultValue = "0") String offset,@RequestParam(defaultValue = "-1") String max) {
 
 		ArrayList<JukeBoxDto> jukeBoxes = new ArrayList<JukeBoxDto>();
 		URL url;
@@ -38,21 +41,27 @@ public class JukeBoxController {
 			HttpURLConnection con;
 			con = (HttpURLConnection) url.openConnection();
 			con.setRequestMethod("GET");
-			InputStream responseStream=con.getInputStream();
-			ArrayList<JukeBox> jukes = service.processJukeBoxes(responseStream);
+			InputStream inputStream = con.getInputStream();
+			URL urlSettings = new URL("http://my-json-server.typicode.com/touchtunes/tech-assignment/settings");
+			HttpURLConnection conSettings;
+			conSettings = (HttpURLConnection) urlSettings.openConnection();
+			conSettings.setRequestMethod("GET");
+			InputStream inputStreamSettings = conSettings.getInputStream();
+			ArrayList<JukeBox> jukes = service.getJukeBoxes(inputStream,inputStreamSettings,offset,max);
 			for (JukeBox a : jukes) {
 				if (a != null) {
 					jukeBoxes.add(convertToDto(a));
 				}
 			}
+			return new ResponseEntity<>(jukeBoxes, HttpStatus.OK);
 		} catch (Exception e) {
-			e.printStackTrace();
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
 		}
 		
 		
-		return new ResponseEntity<>(jukeBoxes, HttpStatus.OK);
+		
 	}
-
+	
 	private JukeBoxDto convertToDto(JukeBox jukeBox) {
 
 		return new JukeBoxDto(jukeBox.getId(), jukeBox.getModel(), jukeBox.getComponents());
--- JukeBox-Backend/src/main/java/jukeBox/JukeBoxBackend/service/JukeBoxService.java
+++ JukeBox-Backend/src/main/java/jukeBox/JukeBoxBackend/service/JukeBoxService.java
@@ -1,42 +1,85 @@
 package jukeBox.JukeBoxBackend.service;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonToken;
 
 import jukeBox.JukeBoxBackend.model.Components;
 import jukeBox.JukeBoxBackend.model.JukeBox;
+import jukeBox.JukeBoxBackend.model.Requirement;
+import jukeBox.JukeBoxBackend.model.Settings;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
 
 
 @Service
 public class JukeBoxService {
 
-	public ArrayList<JukeBox> processJukeBoxes(InputStream responseStream) {
-//		JsonParser jsonParser;
+	public ArrayList<JukeBox> getJukeBoxes(InputStream inputStream,InputStream inputStreamSettings,String shift,String max) throws Exception {
+		try {
+			Integer offset = Integer.parseInt(shift);
+			try {
+				Integer limit = Integer.parseInt(max);
+				try {
+					ArrayList<JukeBox> allJukeBoxes = generateJukeBoxes(inputStream);
+					try {
+						ArrayList<Settings> settings = generateSettings(inputStreamSettings);
+						return allJukeBoxes;
+						//if (offset>array.size()) throw new Exception("OffSet Value is too Big");
+					} catch (Exception e) {
+						throw new Exception(e.getMessage());
+					}
+				} catch (Exception e) {
+					throw new Exception(e.getMessage());
+				}
+		} catch(Exception e) {
+			throw new Exception("Limit value is not an acceptable number. "+e.getMessage());
+		}
+		
+		} catch(Exception e) {
+			throw new Exception("Offset value is not an acceptable number. "+e.getMessage());
+		}
+		
+	}
+	private ArrayList<Settings> generateSettings(InputStream inputStreamSettings) {
+		ArrayList<Settings> settings = new ArrayList<Settings>();
+		JsonReader fileReader = Json.createReader(inputStreamSettings);
+		JsonObject object = fileReader.readObject();
+		JsonArray array=object.getJsonArray("settings");
+		for (int j=0;j<array.size();j++) {
+			ArrayList<Requirement> requirements = new ArrayList<Requirement>();
+			JsonArray requirementsToGet =array.getJsonObject(j).getJsonArray("requires");
+			for (int i=0;i<requirementsToGet.size();i++) {
+				requirements.add(new Requirement(requirementsToGet.getString(i)));
+			}
+			Settings setting = new Settings(array.getJsonObject(j).getString("id"),requirements);
+		    //offset and limit
+			settings.add(setting);
+		
+		}
+		return settings;
+	}
+	private ArrayList<JukeBox> generateJukeBoxes(InputStream inputStream) throws Exception{
 		ArrayList<JukeBox> jukeBoxes = new ArrayList<JukeBox>();
-//		try {
-//			jsonParser = new JsonFactory().createParser(responseStream);
-//			while(jsonParser.nextToken() != JsonToken.END_OBJECT) {
-//				jsonParser.getCurrentName() + ":" +      jsonParser.getCurrentValue());
-		Components component = new Components("led_panel");
-		ArrayList<Components> components = new ArrayList<Components>();
-		components.add(component);
-		    JukeBox juke = new JukeBox("1","fus",components);
-//		}
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-		    //off set and limit
-		jukeBoxes.add(juke);
+		JsonReader fileReader = Json.createReader(inputStream);
+		JsonArray array = fileReader.readArray();
+		for (int j=0;j<array.size();j++) {
+			ArrayList<Components> components = new ArrayList<Components>();
+			JsonArray componentsToGet =array.getJsonObject(j).getJsonArray("components");
+			for (int i=0;i<componentsToGet.size();i++) {
+				components.add(new Components(componentsToGet.getJsonObject(i).getString("name")));
+			}
+			JukeBox juke = new JukeBox(array.getJsonObject(j).getString("id"),array.getJsonObject(j).getString("model"),components);
+		    //offset and limit
+			jukeBoxes.add(juke);
+		
+		}
 		return jukeBoxes;
 	}
+
 	
 }
